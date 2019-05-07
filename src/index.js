9b15e658ac1441cd529b2e1b9afb4679c0c50dc8
Samir Mehta <samir.mehta@consensys.net>
2019-05-07 18:19:37 -0400

    export NetworkConnectivity component

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 module.exports = {
   get NetworkProvider() {
-    return require('./components/NetworkProvider').default;
+    return require("./components/NetworkProvider").default;
   },
   get ReduxNetworkProvider() {
-    return require('./components/ReduxNetworkProvider').default;
+    return require("./components/ReduxNetworkProvider").default;
   },
   get NetworkConsumer() {
-    return require('./components/NetworkConsumer').default;
+    return require("./components/NetworkConsumer").default;
   },
   get reducer() {
-    return require('./redux/reducer').default;
+    return require("./redux/reducer").default;
   },
   get createNetworkMiddleware() {
-    return require('./redux/createNetworkMiddleware').default;
+    return require("./redux/createNetworkMiddleware").default;
   },
   get offlineActionTypes() {
-    return require('./redux/actionTypes').default;
+    return require("./redux/actionTypes").default;
   },
   get networkSaga() {
-    return require('./redux/sagas').default;
+    return require("./redux/sagas").default;
   },
   get checkInternetConnection() {
-    return require('./utils/checkInternetConnection').default;
+    return require("./utils/checkInternetConnection").default;
   },
+  get NetworkConnectivity() {
+    return require("./components/NetworkConnectivity").default;
+  }
 };
